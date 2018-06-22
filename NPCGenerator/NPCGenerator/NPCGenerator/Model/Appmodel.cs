787306241e8d7eaa4cfb84ed32ddc0ed73198d2a
Romy Spangenberg <romy.spangenberg@stud.htwk-leipzig.de>
2018-06-22 22:07:32 +0200

    Add model classes

---
+++ NPCGenerator/NPCGenerator/NPCGenerator/Model/Appmodel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace NPCGenerator.Model
+{
+    class AppModel
+    {
+        public GenreModel CurrentGenre { get; set; }
+        public ObservableCollection<GenreModel> Genres { get; set; }
+        public ObservableCollection<PropertyModel> AllProperties { get; set; }
+    }
+}
---
+++ NPCGenerator/NPCGenerator/NPCGenerator/Model/PropertyTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPCGenerator.Model
+{
+    enum PropertyTypes
+    {
+        String,
+        Int,
+        Colour,
+        Date,
+        Boolean
+    }
+}
