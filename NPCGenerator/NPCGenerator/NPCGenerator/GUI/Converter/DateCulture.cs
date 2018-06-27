e3e5bbb49abe893de2fc7c33f3ee1f51d2d0565b
tobiv <tobivanhelsinki@live.de>
2018-06-27 23:15:48 +0200

    much fixes with date ui

---
+++ NPCGenerator/NPCGenerator/NPCGenerator/GUI/Converter/DateCulture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace NPCGenerator.GUI.Converter
+{
+    // Custom class implements the IValueConverter interface. 
+    public class DateCulture : IValueConverter
+    {
+        #region IValueConverter Members 
+        public object Convert(object value, Type targetType, object parameter, CultureInfo language)
+        {
+            if (value is DateTime)
+            {
+                return ((DateTime)value).ToString(CultureInfo.CurrentCulture.DateTimeFormat.LongDatePattern);
+            }
+            return "FORMAT ERROR";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo language)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
